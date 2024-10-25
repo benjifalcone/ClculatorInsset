31216f438491172143f71586f52fd22ca3aa8f2d
Syhnzz <jeremy.quinol@gmail.com>
2024-10-25 12:11:19 +0200

    Nouvelle page

---
+++ src/main/java/org/insset/server/PourcentServiceImpl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.insset.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import org.insset.client.service.PourcentageService;
+
+/**
+ *
+ * @author insset
+ */
+@SuppressWarnings("serial")
+public class PourcentServiceImpl extends RemoteServiceServlet implements
+        PourcentageService {
+
+
+}
